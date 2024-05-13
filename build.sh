6dd4a45cf291b835e38f02ca696adc40c5fadaf9
Kweku7k <mr.adumatta@gmail.com>
2024-05-13 15:00:28 +0000

    upload difference

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ python3 -m venv $venv_name
 source $venv_name/bin/activate
 
 # Install dependencies from requirements.txt
-pip3 install -r required.txt
+pip3 install -r requirements.txt
 
 # Install uwsgi
 pip install uwsgi
